29c9914709d592fefcbf5075073bc9f8834cc413
Heather Mathies <heathermathies@Heathers-MacBook-Air.local>
2017-08-15 09:49:33 -0400

    moved js into separate file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,54 @@
+
       var s;
       var count = 0;
       var answerArray =[];
+      var selectedLetters =[];
       var randomWordArr = ["pizza", "cake", "cookies", "candy", "scones"];
-    var randomWord = randomWordArr[Math.floor(Math.random() * randomWordArr.length)];
+      var randomWord = randomWordArr[Math.floor(Math.random() * randomWordArr.length)];
+
+      String.prototype.replaceAt = function(index, replacement) {
+        return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+      }
       
-    //below is the code that will put the dashes for whatever word
-    //math.random choose from the array at the start of the game
+      function startUp() {
       
-      function startUp()
-      {
-        for (var i = 0; i< randomWord.length; i++)
-        {
+        for (var i = 0; i< randomWord.length; i++) {
           answerArray[i] = "-";
         }
-       
-        s = answerArray.join(" ");
+        s = answerArray.join("");
         document.getElementById("answer").innerHTML = s;
       }
+      startUp();
 
-      //stuck on how to get the user guesses, when correct, on the line
-      //without the line disappearing
+      function addLetter(){
+        // Grabbing the letter from the input value
+        var letter = document.getElementById("letter").value;
+
+        // is the letter selected included in the random word
+        if(randomWord.indexOf(letter) > -1) {
+          // Get the current status of the answer
+          var currentAnswer = document.getElementById("answer").innerHTML;
+          // Get the position of where the letter is found in the random word
+          var currentIndexOrLetter = randomWord.indexOf(letter);
+          // Update the string to include the letter selected
+          var newPartialAnswerString = currentAnswer.replaceAt(currentIndexOrLetter, letter);
+          //Update the page (html) to have the selected letter
+          document.getElementById("answer").innerHTML = newPartialAnswerString;
+          // Clear the value from the field
+          document.getElementById("letter").value = ""
+
+          if(newPartialAnswerString.indexOf('-') === -1){
+            console.log('YOU WIN!! You successfully guessed the word ' + randomWord + '!');
+          }
+        }else{
+          // Alert the user if there is no match
+          alert('There was no ' + letter + ' found in the word');
+        }
+      }
+
+
+
+  
+
       
         
